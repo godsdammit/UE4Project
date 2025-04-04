3ab81beca1f4f69bd84203484279ffef93ba91e7
Gary Erickson <ericksong@me.com>
2025-04-04 16:39:37 -0400

    Creating Unreal Engine 4 Project also

---
+++ Source/UE4VersionControl/UE4VersionControl.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4VersionControl.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4VersionControl, "UE4VersionControl" );
