409a663172a798712db42cb378e25f53c51be021
Gary Erickson <ericksong@me.com>
2025-04-04 18:52:12 -0400

    get mesh component

--- Source/UE4VersionControl/Prop.h
+++ Source/UE4VersionControl/Prop.h
@@ -23,7 +23,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-private:
 	UPROPERTY(EditAnywhere)
-	class UStaticMeshComponent* MeshComp;
+	class UStaticMeshComponent* MeshComp; // Static mesh component to represent the prop
+	
 };
